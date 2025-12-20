b16ca3c8be3c3ca5d577146d2a6ef7ace60a9bf5
JackA1ltman <cs2dtzq@163.com>
2025-12-20 09:52:10 +0800

    mk: changed ofox to twrp
    Change-Id: I703c48d83eb64183bac495f25344e8a9dbbbe65b
    Signed-off-by: JackA1ltman <cs2dtzq@163.com>

--- twrp_opkona.mk
+++ twrp_opkona.mk
@@ -22,7 +22,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := ofox_$(PRODUCT_DEVICE)
+PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
 PRODUCT_BRAND := oneplus
 PRODUCT_MODEL := IN2010
 PRODUCT_MANUFACTURER := oneplus
